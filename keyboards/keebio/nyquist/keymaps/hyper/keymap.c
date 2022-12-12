6c0cdb912672906d5a6f89de00cc6119bc74e7a2
arrowtype <arrowtypeco@gmail.com>
2022-12-12 17:28:51 -0500

    move hyper key to caps lock / backtick position

--- keyboards/keebio/nyquist/keymaps/hyper/keymap.c
+++ keyboards/keebio/nyquist/keymaps/hyper/keymap.c
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 [_QWERTY] = LAYOUT(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                  KC_6,    KC_7,    KC_8,                  KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                  KC_Y,    KC_U,    KC_I,                  KC_O,    KC_P,    KC_QUOT,
-  KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                  KC_H,    KC_J,    KC_K,                  KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                  KC_N,    KC_M,    KC_COMM,               KC_DOT,  KC_UP,   KC_RSFT,
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   MT(MOD_HYPR, KC_SPC),  KC_SPC,  RAISE,   MT(MOD_RGUI, KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
+  KC_ESC,                KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,                  KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,                KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,                  KC_O,    KC_P,    KC_BSLS,
+  MT(MOD_HYPR, KC_GRV),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,                  KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,               KC_DOT,  KC_UP,   MT(MOD_RSFT, KC_ENT),
+  ADJUST,                KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   MT(MOD_RGUI, KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Colemak
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK_DH] = LAYOUT(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                  KC_6,    KC_7,    KC_8,                  KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                  KC_J,    KC_L,    KC_U,                  KC_Y,    KC_SCLN, KC_QUOT,
-  KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                  KC_M,    KC_N,    KC_E,                  KC_I,    KC_O,    KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                  KC_K,    KC_H,    KC_COMM,               KC_DOT,  KC_UP,   KC_RSFT,
-  ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   MT(MOD_HYPR, KC_SPC),  KC_SPC,  RAISE,   MT(MOD_RGUI, KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
+  KC_ESC,                KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,                KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+  MT(MOD_HYPR, KC_GRV),  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+  KC_LSFT,               KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_UP,   MT(MOD_RSFT, KC_ENT),
+  ADJUST,                KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   MT(MOD_RGUI, KC_SLSH), KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Lower
